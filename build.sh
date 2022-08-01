82da1124aee3d9000e4a64419996bafa7ddc9611
İsmail <84140360+Headache01@users.noreply.github.com>
2022-08-01 20:32:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+# Fix needed for kernel
+cd /tmp/rom/prebuilts/tools-extras/common/perl-base/Getopt
+wget https://raw.githubusercontent.com/Headache01/notes/main/Std.pm
+cd /tmp/rom
+
 # Normal build steps
+
 . build/envsetup.sh
 lunch havoc_GM8_sprout-userdebug
 
